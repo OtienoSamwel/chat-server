40ff4e36558f33f2a6a2c4072fa5a4a9993a9dd5
OtienoSamwel <otienosamwel135@gmail.com>
2022-04-09 22:49:22 +0300

    migrate to ktor 2.0

--- src/test/kotlin/com/otienosamwel/ApplicationTest.kt
+++ src/test/kotlin/com/otienosamwel/ApplicationTest.kt
@@ -1,15 +1,6 @@
 package com.otienosamwel
 
-import io.ktor.routing.*
 import io.ktor.http.*
-import io.ktor.http.cio.websocket.*
-import io.ktor.websocket.*
-import java.time.*
-import io.ktor.features.*
-import org.slf4j.event.*
-import io.ktor.request.*
-import io.ktor.application.*
-import io.ktor.response.*
 import kotlin.test.*
 import io.ktor.server.testing.*
 import com.otienosamwel.plugins.*
